ad61c5379fdc2206797b7efb2e93e0ffc04ccc82
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-07-24 14:07:46 -0700

    create guessing game with forms

--- app/controllers/query_params_controller.rb
+++ app/controllers/query_params_controller.rb
@@ -22,8 +22,40 @@ class QueryParamsController < ApplicationController
     end
   end
 
+  def guess_number_url
+    @number = params[:number].to_i
+    winning_number = 91
+    if @number < winning_number
+      @message = "You LOSE! Guess higher yo!"
+    elsif @number > winning_number
+      @message = "You LOSE! Guess lower yo!"
+    elsif @number == winning_number
+      @message = "You Win! Congrats... I guess..."
+    end
+  end
+
   def button_counter
     @counter = params[:count].to_i
+  end
+
+  def globbing
+    @directory = params[:pict]
+  end
+
+  def guess_number_form
+
+  end
+
+  def guess_number_target
+    @number = params[:number].to_i
+    @winning_number = 64
+    if @number < @winning_number
+      @message = "You LOSE! Guess higher stoopid!"
+    elsif @number > @winning_number
+      @message = "You LOSE! Guess lower nincompoop!"
+    elsif @number == @winning_number
+      @message = "You Win! Congrats... I guess..."
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   get '/name', to: 'query_params#name'
   get '/guess_a_number', to: 'query_params#guess_a_number'
   get '/button_counter', to: 'query_params#button_counter'
+  get '/guess_number_url/:number', to: 'query_params#guess_number_url'
+  get '/globbing/*pict', to: 'query_params#globbing'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/guess_number_form', to: 'query_params#guess_number_form'
+  post '/guess_number_target', to: 'query_params#guess_number_target'
 end
