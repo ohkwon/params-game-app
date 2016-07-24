89f3030100d618ed877e5d432ef5554fd9f45499
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-07-24 09:32:22 -0700

    create guess_a_number game

--- app/controllers/query_params_controller.rb
+++ app/controllers/query_params_controller.rb
@@ -4,4 +4,10 @@ class QueryParamsController < ApplicationController
     @first_name = params[:first_name]
     @last_name = params[:last_name]
   end
+
+  def guess_a_number
+    @number = params[:number].to_i
+    @winning_number = 25
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
   get '/name', to: 'query_params#name'
+  get '/guess_a_number', to: 'query_params#guess_a_number'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
