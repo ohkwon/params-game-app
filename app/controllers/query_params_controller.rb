f4d56ebb93c02cc16f28297884d86baeb192b26f
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-07-24 10:49:09 -0700

    fix code so all logic in controller. solve button counter problem

--- app/controllers/query_params_controller.rb
+++ app/controllers/query_params_controller.rb
@@ -1,13 +1,29 @@
 class QueryParamsController < ApplicationController
 
   def name
-    @first_name = params[:first_name]
-    @last_name = params[:last_name]
+    @first_name = params[:first_name].upcase
+    @last_name = params[:last_name].upcase
+    if @first_name.start_with?("A")
+      @message = "Hey, your name starts with the first letter of the alphabet!"
+    else
+      @message = "Hey, get a better name!"
+    end
   end
 
   def guess_a_number
     @number = params[:number].to_i
-    @winning_number = 25
+    winning_number = 25
+    if @number < winning_number
+      @message = "You LOSE! Guess higher yo!"
+    elsif @number > winning_number
+      @message = "You LOSE! Guess lower yo!"
+    elsif @number == winning_number
+      @message = "You Win! Congrats... I guess..."
+    end
+  end
+
+  def button_counter
+    @counter = params[:count].to_i
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
   get '/name', to: 'query_params#name'
   get '/guess_a_number', to: 'query_params#guess_a_number'
+  get '/button_counter', to: 'query_params#button_counter'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
